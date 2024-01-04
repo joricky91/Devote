2448a489856dec3cc6ce9a7abddc7bc2c7995fd3
joricky91 <jonathanricky91@gmail.com>
2024-01-04 13:31:31 +0700

    feat: setup core data

---
+++ Devote/Utilities/Constant.swift
@@ -0,0 +1,23 @@
+//
+//  Constant.swift
+//  Devote
+//
+//  Created by Jonathan Ricky Sandjaja on 04/01/24.
+//
+
+import Foundation
+import SwiftUI
+
+//MARK: - FORMATTER
+let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+//MARK: - UI
+
+
+//MARK: - UX
+
