a361efd28928fcb7d06207866096eca178b2fa01
joricky91 <jonathanricky91@gmail.com>
2024-01-05 17:09:29 +0700

    feat: finishing widget configuration

---
+++ DevoteWidget/DevoteWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  DevoteWidgetBundle.swift
+//  DevoteWidget
+//
+//  Created by Jonathan Ricky Sandjaja on 05/01/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct DevoteWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        DevoteWidget()
+    }
+}
